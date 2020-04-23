031c019faa13710bc36b4c375040f597ba42369b
0xBono <0xbono@naver.com>
2020-04-24 03:37:16 +0900

    chore: file folder setting

---
+++ src/components/Category.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  TextContainer: {
+
+  }
+});
+
+class Category extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+          <View style={styles.TextContainer}>
+              
+          </View>
+      </View>
+    );
+  }
+}
+
+export default Category;
