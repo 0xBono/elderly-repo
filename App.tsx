072624e9084ce764541c7f4620f09548d97c1793
0xBono <0xbono@naver.com>
2020-04-24 03:36:34 +0900

    feat: development tab navigation

--- App.tsx
+++ App.tsx
@@ -1,118 +1,87 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React, {useRef} from 'react'; // 프레임 워크
+import {View, TouchableOpacity, Text, Dimensions} from 'react-native'; // 프레임워크
+import {NavigationContainer} from '@react-navigation/native'; // 이동 라우터 설정
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'; // 하단 탭
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {Modalize} from 'react-native-modalize'; // 모달
+import {Host, Portal} from 'react-native-portalize'; // 모달 필요한 라이브러리
+import MapView, {Maker, PROVIDER_GOOGLE} from 'react-native-maps'; // 구글 맵
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+const {width, height} = Dimensions.get('window'); //스크린 가로 세로
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const SPACE = 0.01;
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Tab = createBottomTabNavigator();
 
-declare var global: {HermesInternal: null | {}};
+const ExamplesScreen = () => {
+  const modalRef = useRef<Modalize>(null);
 
-const App = () => {
+  const onOpen = () => {
+    modalRef.current?.open();
+  };
+
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+      <TouchableOpacity onPress={onOpen}>
+        <Text style={{marginTop: 100}}>Open the modal</Text>
+      </TouchableOpacity>
+
+      <Portal>
+        <Modalize snapPoint={384} ref={modalRef}>
+          <Text>...your content</Text>
+        </Modalize>
+      </Portal>
     </>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const CategoryScreen = () => (
+  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text>Category</Text>
+  </View>
+);
+
+const App = () => (
+  <NavigationContainer>
+    <Host>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Examples"
+          component={ExamplesScreen}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Category"
+          component={CategoryScreen}
+          options={{
+            tabBarLabel: 'Category',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons name="menu" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Setting"
+          component={CategoryScreen}
+          options={{
+            tabBarLabel: 'Setting',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons name="alert" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </Host>
+  </NavigationContainer>
+);
 
 export default App;
